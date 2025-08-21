9c26f00d6b3e58365fc687c14e78ad272a6719a3
S-Katagiri <katagiri.stsh@gmail.com>
2025-08-21 22:43:17 +0900

    JSON parser

---
+++ src/JsonParser.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using TaleWorlds.Localization;
+
+namespace NobleTitlesPlus.json
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ClanNamePair
+    {
+        [JsonConstructor]
+        public ClanNamePair(string name_short = "", string name_long = "")
+        {
+            this.ClanShort = new(name_short);
+            this.ClanLong = new(name_long);
+        }
+        public ClanNamePair(TextObject? name_short, TextObject? name_long)
+        {
+            this.ClanShort = name_short ?? new("");
+            this.ClanLong = name_long ?? new("");
+        }
+        [JsonProperty("name_short")]
+        public TextObject ClanShort { get; set; } = new();
+        [JsonProperty("name_long")]
+        public TextObject ClanLong { get; set; } = new();
+    }
+}
